f125a2315280fa009ed94fe31a8bd17088a8b937
rajmanna <dev.rajm8923@gmail.com>
2025-08-03 12:28:42 +0530

    help_me_unpack

---
+++ help_me_unpack/script.js
@@ -0,0 +1,29 @@
+import { Buffer } from "node:buffer";
+import axios from "axios";
+
+(async () => {
+  // Get the problem json
+  const { data } = await axios.get(
+    "https://hackattic.com/challenges/help_me_unpack/problem?access_token=9def105d03dce269"
+  );
+
+  // Convert base64 to raw bytes blob
+  const buf = Buffer.from(data.bytes, "base64");
+
+  // Build the solution
+  const result = {
+    int: buf.readInt32LE(0), // 0 - 3
+    uint: buf.readUInt32LE(4), // 4 - 7
+    short: buf.readInt32LE(8), // 8 - 11
+    float: buf.readFloatLE(12), // 12 - 15
+    double: buf.readDoubleLE(16), // 16 - 23
+    big_endian_double: buf.readDoubleBE(24), // 24 - 31
+  };
+
+  // Submit the solution
+  const solve = await axios.post(
+    "https://hackattic.com/challenges/help_me_unpack/solve?access_token=9def105d03dce269",
+    result
+  );
+  console.log(solve.data);
+})();
