e92f03a636a2d2ce3754869bff37c6dad8709d80
rajmanna <dev.rajm8923@gmail.com>
2025-08-05 19:07:10 +0530

    my first attempt

--- backup_restore/script.js
+++ backup_restore/script.js
@@ -1,11 +1,29 @@
 import axios from "axios";
-import { execSync } from "child_process";
+import { Client } from "pg";
 import { config } from "dotenv";
 import { writeFile } from "fs";
 import path from "path";
 import { gunzip } from "zlib";
 
 config({ path: "../.env", quiet: true });
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  port: process.env.PORT,
+});
+await client.connect();
+
+async function getData() {
+  const query = "SELECT ssn FROM criminal_records WHERE status=$1";
+  const { rows } = await client.query(query, ["alive"]);
+
+  const aliveSSNs = rows.map((row) => row.ssn);
+
+  return aliveSSNs;
+}
 
 (async () => {
   const response = await axios.get(
@@ -14,20 +32,20 @@ config({ path: "../.env", quiet: true });
   const { dump } = response.data; // Get the postgres database dump from the problem
 
   const buf = Buffer.from(dump, "base64"); // Store the dump as buffer
-  console.log(buf);
+  // console.log(buf);
 
   const __dirname = path.resolve();
   const sqlPath = path.resolve(__dirname, "dump.sql");
 
   // Decompress the dump buffer
-  gunzip(buf, (err, decompressBuf) => {
+  gunzip(buf, async (err, decompressBuf) => {
     if (err) {
       console.log(`Error while decompressing: ${err}`);
       return;
     }
 
     const sql = decompressBuf.toString("utf8"); // Decompressed SQL dump
-    console.log(sql);
+    // console.log(sql);
 
     // Create the dump.sql file
     writeFile(sqlPath, sql, (err) => {
@@ -39,6 +57,21 @@ config({ path: "../.env", quiet: true });
       console.log("Writing sql file successfully");
     });
 
-    execSync(``);
+    // psql -U postgres -d hackattic -f dump.sql (manually)
+
+    setTimeout(async () => {
+      // DB Query
+      const SSNs = await getData();
+      const result = { alive_ssns: SSNs };
+      console.log(result);
+
+      const solve = await axios.post(
+        `https://hackattic.com/challenges/backup_restore/solve?access_token=${process.env.TOKEN}`,
+        result
+      );
+
+      console.log(`Result: ${solve.data}`);
+      await client.end();
+    }, 50000);
   });
 })();
