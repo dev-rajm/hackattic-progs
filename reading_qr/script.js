32912b0dfd99e664f8b0374414600f5afe37e5c8
rajmanna <dev.rajm8923@gmail.com>
2025-08-06 11:18:17 +0530

    optimized reading_qr

--- reading_qr/script.js
+++ reading_qr/script.js
@@ -5,35 +5,62 @@ import QrCode from "qrcode-reader";
 
 dotenv.config({ quiet: true, path: "../.env" });
 
-(async () => {
-  const { data } = await axios.get(
-    `https://hackattic.com/challenges/reading_qr/problem?access_token=${process.env.TOKEN}`
-  );
-  console.log(data); // Problem JSON
-
-  const response = await axios.get(data.image_url, {
-    responseType: "arraybuffer",
-  });
-
-  const buffer = response.data; // Store the qr as buffer
-  console.log(buffer);
-
-  const image = await Jimp.read(buffer); // Reading image buffer
-
-  const qr = new QrCode();
-  qr.callback = async (err, value) => {
-    if (err) {
-      console.error(`Error while decoding qr: ${err}`);
-      return;
-    }
-    console.log(`Decoded value: ${value.result}`);
-
-    // Submit the solution JSON {code: xx-xxx-xx-xxx}
-    const solve = await axios.post(
-      `https://hackattic.com/challenges/reading_qr/solve?access_token=${process.env.TOKEN}`,
-      { code: value.result }
+// Extract image data from problem JSON
+async function extractImageData() {
+  try {
+    const { data } = await axios.get(
+      `https://hackattic.com/challenges/reading_qr/problem?access_token=${process.env.TOKEN}`
     );
-    console.log(solve.data); // Result
-  };
-  qr.decode(image.bitmap); // core decoding
-})();
+
+    const response = await axios.get(data.image_url, {
+      responseType: "arraybuffer",
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      `Failed to extract image from problem data: ${error.message}`
+    );
+  }
+}
+
+// Extract code from qr
+async function getCodeFromImage(buffer) {
+  try {
+    const image = await Jimp.read(buffer);
+    const qr = new QrCode();
+
+    qr.callback = (err, value) => {
+      if (err) {
+        console.error(`Error while decoding qr: ${err}`);
+        return;
+      }
+      return value.result;
+    };
+    qr.decode(image.bitmap); // core decoding
+  } catch (error) {
+    throw new Error(`Failed to extract code from qr: ${error.message}`);
+  }
+}
+
+// Submit solution
+async function submitResult(result) {
+  try {
+    const { data } = await axios.post(
+      `https://hackattic.com/challenges/reading_qr/solve?access_token=${process.env.TOKEN}`,
+      result
+    );
+    console.log(`Hackattic response: ${data.message}`);
+  } catch (error) {
+    throw new Error(`Failed to submit solution: ${error.message}`);
+  }
+}
+
+async function main() {
+  const imageBuffer = await extractImageData();
+  const code = await getCodeFromImage(imageBuffer);
+
+  await submitResult({ code: code });
+}
+
+main();
