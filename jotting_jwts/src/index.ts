166fb970e6d366986343a38d88a7f6eb2e9ecd71
rajmanna <dev.rajm8923@gmail.com>
2025-10-06 19:58:32 +0530

    added jwt

--- jotting_jwts/src/index.ts
+++ jotting_jwts/src/index.ts
@@ -1,7 +1,20 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
 import { getProblemJSON, submitSolution } from './packages/setup';
 
 async function main() {
-  const jwt_secret = await getProblemJSON();
+  const str = "";
+  const jwt = await getProblemJSON();
+
+  const app = express();
+
+  app.post("/", (req, res) => {
+    const token = req.body;
+    res.send("Received post request at /");
+  })
+
+  app.listen(3000);
 }
 
 main();
