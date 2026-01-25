ff318c8648a0db428b4a581cf747a0c15bb638ed
dev-rajm <dev.rajm8923@gmail.com>
2026-01-25 14:05:23 +0530

    brute-force-zip/writting the response

--- brute_force_zip/main.c
+++ brute_force_zip/main.c
@@ -1,47 +1,71 @@
+#include <curl/curl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <curl/curl.h>
 
-size_t write_data_callback(void *buffer, size_t size, size_t nmemb, void *userp) {
-  size_t written = fwrite(buffer, size, nmemb, (FILE *)userp);
-  return written;
+typedef struct memory {
+  char *data;
+  size_t size;
+} memory_t;
+
+size_t write_data(void *contents, size_t size, size_t nmemb, void *userp) {
+  size_t real_size = size * nmemb;
+  memory_t *mem = (memory_t*)userp;
+
+  char *ptr = realloc(mem->data, mem->size + real_size +1);
+  if(!ptr) {
+    return 0;
+  }
+
+  mem->data = ptr;
+  memcpy(&(mem->data[mem->size]), contents, real_size);
+  mem->size += real_size;
+  mem->data[mem->size] = '\0';
+
+  return real_size;
 }
 
 int main(void) {
   CURL *curl;
-  CURLcode res = curl_global_init(CURL_GLOBAL_ALL);
-
-  char *TOKEN = getenv("HACKATTIC_TOKEN");
-
-  char *url = "https://hackattic.com/challenges/brute_force_zip/problem?access_token=";
-  strcat(url, TOKEN);
-  char *output_filename = "protected.zip";
-
-  curl = curl_easy_init();
-
-  if(curl) {
-    FILE *file = fopen(output_filename, "wb");
-    if(!file) {
-      fprintf(stderr, "Error opening file %s\n", output_filename);
-      return 1;
-    }
-
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
-
-    res = curl_easy_perform(curl);
-
-    if(res != CURLE_OK) {
-      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-    }
-
-    fclose(file);
-    curl_easy_cleanup(curl);
+  CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
+  if (result) {
+    return (int)result;
   }
 
-  curl_global_cleanup();
+  memory_t response;
+  response.data = malloc(1);
+  response.size = 0;
 
+  const char *TOKEN = getenv("HACKATTIC_TOKEN");
+  const char *base =
+      "https://hackattic.com/challenges/brute_force_zip/problem?access_token=";
+
+  size_t len = strlen(base) + strlen(TOKEN) + 1;
+  char *url = malloc(len);
+  if (!url) {
+    return 1;
+  }
+
+  strcpy(url, base);
+  strcat(url, TOKEN);
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    free(url);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    result = curl_easy_perform(curl);
+    if (result != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(result));
+    }
+    printf("Response:\n%s\n", response.data);
+    free(response.data);
+    curl_easy_cleanup(curl);
+  }
+  curl_global_cleanup();
   return 0;
 }
