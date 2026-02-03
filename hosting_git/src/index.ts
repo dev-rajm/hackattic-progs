a64ae5503a56420cb2512c403af35ecc750c6d78
rajmanna <dev.rajm8923@gmail.com>
2026-02-03 15:22:27 +0530

    hosting_git/init project

--- hosting_git/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { getProblemJSON } from "./utils";
-
-async function main(): Promise<void> {
-  const { ssh_key, username, repo_path, push_token } = await getProblemJSON();
-  console.log(username);
-}
-
-main();
